3b8a9fda5872abf88b3e8f19d0b05c981c60ce3f
Luan Arruda <luanmarcosdev@gmail.com>
2025-06-18 08:16:38 -0300

    created user seeders

---
+++ src/seeders/20250618110439-seed-user.cjs
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users_Migrations', [
+      {
+       name: "Luan Marcos",
+       email: "luanmarcosdev@gmail.com",
+       password_hash: "",
+       created_at: new Date()
+      },
+      {
+       name: "Maysa Beatriz",
+       email: "maysabeatriz@gmail.com",
+       password_hash: "",
+       created_at: new Date()
+      },
+      {
+       name: "Lucia Souza",
+       email: "luciasouza@gmail.com",
+       password_hash: "",
+       created_at: new Date()
+      },
+      {
+       name: "Nivanir José",
+       email: "nivanirjose@gmail.com",
+       password_hash: "",
+       created_at: new Date()
+      },
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users_Migrations', null, {});
+  }
+};
