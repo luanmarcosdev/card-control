91a76f5bfaeae99420cdea3680d21cb05a55becd
Luan Arruda <luanmarcosdev@gmail.com>
2025-06-27 16:03:31 -0300

    setuped initial swagger

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import 'dotenv/config';
 import routes from './routes/index.js';
+import fs from 'fs';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const swaggerJson = JSON.parse(fs.readFileSync('./src/swagger.json', 'utf-8'));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJson));
 app.use('/api', routes);
 
 app.listen(PORT, () => {
