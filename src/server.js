51a18c25409ae0211b7b2bdf4bbf10e920e5e8ee
Luan Arruda <luanmarcosdev@gmail.com>
2025-08-12 10:33:03 -0300

    feat(server): add global error handler middleware

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import routes from './routes/index.js';
 import fs from 'fs';
 import swaggerUi from 'swagger-ui-express';
+import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +11,7 @@ const swaggerJson = JSON.parse(fs.readFileSync('./src/swagger.json', 'utf-8'));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJson));
 app.use('/api', routes);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
